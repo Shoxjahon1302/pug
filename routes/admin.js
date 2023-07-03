0188223a6d9680ccf396a505e1a9778c0c4d92b4
Shoxjahon <shoxjahonniyatov@gmail.com>
2023-07-03 16:29:02 +0500

    Finished Front zone

--- routes/admin.js
+++ routes/admin.js
@@ -3,4 +3,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.render("admin/index", { title: "Dashboard" });
 });
+router.get("/product", (req, res) => {
+  res.render("admin/pages/product/index", { title: "Products" });
+});
 module.exports = router;
